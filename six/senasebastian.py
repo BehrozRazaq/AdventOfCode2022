8104304c4cf95c63d31d64445574c53d3ebf3bd0
BehrozRazaq <behroz.razaq@gmail.com>
2022-12-06 11:00:32 +0100

    Removed unused row

--- six/senasebastian.py
+++ six/senasebastian.py
@@ -2,9 +2,11 @@ file = open("six\input.txt")
 line = file.readline()
 
 previousThree = line[:3]
-hasDuplicates = 0
 start = 0
 for charIndex in range(len(line)):
+    hasDuplicates = 0
+    for previous in previousThree:
+        hasDuplicates += int(previousThree.count(previous) > 1)
     char = line[charIndex]
     if char in previousThree or hasDuplicates:
         previousThree = previousThree[1:]
@@ -12,9 +14,6 @@ for charIndex in range(len(line)):
     else:
         start = charIndex + 1
         break
-    hasDuplicates = 0
-    for previous in previousThree:
-        hasDuplicates += int(previousThree.count(previous) > 1)
         
                 
     
--- six/zoomingsebastian.py
+++ six/zoomingsebastian.py
@@ -2,9 +2,11 @@ file = open("six\input.txt")
 line = file.readline()
 
 previousForteen = line[:13]
-hasDuplicates = 0
 start = 0
 for charIndex in range(len(line)):
+    hasDuplicates = 0
+    for previous in previousForteen:
+        hasDuplicates += int(previousForteen.count(previous) > 1)
     char = line[charIndex]
     if char in previousForteen or hasDuplicates:
         previousForteen = previousForteen[1:]
@@ -12,9 +14,6 @@ for charIndex in range(len(line)):
     else:
         start = charIndex + 1
         break
-    hasDuplicates = 0
-    for previous in previousForteen:
-        hasDuplicates += int(previousForteen.count(previous) > 1)
         
                 
     
